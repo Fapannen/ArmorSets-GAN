Redo normalization to [-1, 1], fix grayscale inspector if needed

inspector.py
@@ -2,6 +2,7 @@ from keras.models import load_model
 from matplotlib import pyplot as plt
 from preprocessing import generator
 from hparams import hparams
+import numpy as np
 import cv2
 
 config = hparams.Config()
@@ -14,6 +15,6 @@ X = model.predict(latent_points)
 # plot the result
 for i in range(len(X)):
     if config.MODE == cv2.IMREAD_COLOR:
-        plt.imsave("trained_and_generated" + str(i) + ".jpg", X[i])
+        cv2.imwrite("trained_and_generated" + str(i) + ".jpg", (np.array(X[i]) * 127.5) + 127.5)
     if config.MODE == "GRAYSCALE":
         plt.imsave("trained_and_generated" + str(i) + ".jpg", X[i, :, :, 0], cmap='gray')

main.py
@@ -19,11 +19,11 @@ def main():
 
     # Draw an example images
     if config.IMG_CHANNELS == 3:
-        cv2.imwrite('test1.png', np.array(images[58, :, :, :, 0]) * 255)
-        cv2.imwrite('test2.png', np.array(images[59, :, :, :, 0]) * 255)
+        cv2.imwrite('test1.png', (np.array(images[58, :, :, :, 0]) * 127.5) + 127.5)
+        cv2.imwrite('test2.png', (np.array(images[59, :, :, :, 0]) * 127.5) + 127.5)
     else: # if grayscale
-        cv2.imwrite('test1.png', np.array(images[58]) * 255)
-        cv2.imwrite('test2.png', np.array(images[59]) * 255)
+        cv2.imwrite('test1.png', (np.array(images[58]) * 127.5) + 127.5)
+        cv2.imwrite('test2.png', (np.array(images[59]) * 127.5) + 127.5)
 
     # define the generator with target IMG height and IMG width
     gan_generator = armorGAN.define_generator(config.LATENT_DIM, config.IMG_HEIGHT, config.IMG_WIDTH, config.IMG_CHANNELS)

preprocessing/generator.py
@@ -28,7 +28,7 @@ def generate_real_samples(dataset, n_samples):
 
 def generate_fake_samples(n_samples, img_height, img_width, img_channels):
 	# generate uniform random numbers in [0,1]
-	X = np.random.rand(img_height * img_width * img_channels * n_samples)
+	X = np.random.uniform(low=0.5, high=13.3, size=(img_height * img_width * img_channels * n_samples,))
 	# reshape into a batch of grayscale images
 	if img_channels == 1:
 		X = X.reshape((n_samples, img_height, img_width, img_channels))